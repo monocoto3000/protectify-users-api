4997010cfb5efb1cb3feecc5e8492e47b25de466
monocoto3000 <128664121+monocoto3000@users.noreply.github.com>
2024-07-04 12:44:01 -0600

    dist deleted

--- dist/utils/password.util.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const isPasswordValid = (password) => {
-    return password.length < 8;
-};
-exports.default = isPasswordValid;
